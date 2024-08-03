2a3ed394111173a1a53d17ad1db386d942a2fa68
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2024-08-02 21:46:42 -0300

    update Test_SQL_PostgreSQL.sql

--- Test_SQL_PostgreSQL.sql
+++ Test_SQL_PostgreSQL.sql
@@ -1,20 +1,30 @@
---some test for learning how to migrate
---data from MySQL to PostgreSQL 
+-- some test for learning how to migrate
+-- database queries from MySQL to PostgreSQL 
 
+
+-- Create new table to set level access user
 CREATE TABLE lvl_st(
     id_lvl SERIAL PRIMARY KEY,
-    type_usr VARCHAR(50),
+    type_usr VARCHAR(50) NOT NULL,
 );
 INSERT INTO lvl_st (type_usr)
-VALUES (),(),();
+VALUES 
+('Administrator'),
+('Developer'),
+('Employer'),
+('Guest');
 
+-- Show all level access user
+SELECT * FROM lvl_st;
 
--- Create a new table
+-- Create a new table to add users
 CREATE TABLE user_st(
     id_usr SERIAL PRIMARY KEY,
     name_usr VARCHAR(50),
     pwd_usr VARCHAR(50),
-    lvl_usr INTEGER
+    lvl_usr INTEGER,
+    FOREIGN KEY(lvl_usr)
+    REFERENCES lvl_st(lvl_id)
 );
 
 -- Insert data into the user_st table
@@ -23,5 +33,5 @@ VALUES ('Alias_1', 'password_1', 1),
        ('Alias_2', 'password_2', 2),
        ('Alias_3', 'password_3', 3);
 
--- Retrieve all users
+-- Show all users
 SELECT * FROM user_st;
