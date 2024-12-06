25c9946935b0104aa31a6c808605501dc70823e2
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2024-12-05 22:20:11 -0300

    Atualizar o verify_cpf.py

--- verify_cpf.py
+++ verify_cpf.py
@@ -1,11 +1,10 @@
 values_1=[]
-default_1=[]
+default_1=[10,9,8,7,6,5,4,3,2]
 
-values_2=[]
-default_2=[]
+default_2=[11]+default_1
 
 for i in range(9):
-    x=input(f'Please insert of valeu {i+1}: ')
+    x=input(f'Please insert of value {(i+1)}: ')
     values_1.append(x)
 
 print(values_1)
